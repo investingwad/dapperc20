58c78ed41e107cd73bcc2b0f2e49b91ccdcb4613
shubh singh <ssi@rapidinnovation.dev>
2021-06-15 19:36:16 +0530

    fixed token burn issue

--- README.md
+++ README.md
@@ -7,6 +7,6 @@ This contract has been written by following ERC20 standards, just we have two sp
  
 Bridge Contract Address - 
 Actually bridge contract is also their in openzepllin ERC20 standard there they called it minter,
-so only bridge contract can mint and burn the tokens. Other than minitng and burning bridge contract address can not do anything
+so only bridge contract can mint the tokens. Other than minitng bridge contract address can not do anything
  
 Owner Address - Owner has only two privilege it can update the bridge contract(minter) address and can transfer ownership to some other address, other than that owner cant do anything.
--- contracts/DAPP.sol
+++ contracts/DAPP.sol
@@ -76,7 +76,7 @@ library SafeMath {
 interface IERC20 {
     function balanceOf(address) external view returns (uint256);
     function mint(address, uint256) external;
-    function burn(address, uint256) external;
+    function burn(uint256) external;
     function transfer(address, uint256) external returns (bool);
     function approve(address, uint256) external returns (bool);
     function transferFrom(address, address, uint256) external returns (bool);
@@ -109,11 +109,11 @@ contract DappToken is IERC20 {
         _;
     }
 
-    constructor(string memory _name, string memory _symbol, address _bridgeContractAddress) {
+    constructor(string memory _name, string memory _symbol, address _bridgeContractAddress, address _owner) {
         name = _name;
         symbol = _symbol;
         decimals = 4;
-        ownerAddress = msg.sender;
+        ownerAddress = _owner;
         bridgeContractAddress = _bridgeContractAddress;
     }
 
@@ -158,12 +158,10 @@ contract DappToken is IERC20 {
         emit Transfer(address(0), account, amount);
     }
 
-    function burn(address account, uint256 amount) public override onlyBridge() {
-        require(account != address(0), "ERC20: burn from the zero address");
-  
-        balances[account] = balances[account].sub(amount, "ERC20: burn amount exceeds balance");
+    function burn(uint256 amount) public override {
+        balances[msg.sender] = balances[msg.sender].sub(amount, "ERC20: burn amount exceeds balance");
         totalSupply = totalSupply.sub(amount);
-        emit Transfer(account, address(0), amount);
+        emit Transfer(msg.sender, address(0), amount);
     }
 
     function _approve(address owner, address spender, uint256 amount) internal virtual {
