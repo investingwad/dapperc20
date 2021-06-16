d9586e2a1cb14906d8113864dc2c17b2808fc65c
Lucas Cullen <bitcoinbrisbane@users.noreply.github.com>
2021-06-17 09:25:04 +1000

    Chore: Fix linting

--- contracts/DAPP.sol
+++ contracts/DAPP.sol
@@ -176,10 +176,9 @@ contract DappToken is IERC20 {
         require(_bridgeContractAddress != address(0), "Bridge address is zero address");
         bridgeContractAddress = _bridgeContractAddress;
     }
+    
     function transferOwnership(address _newOwner) public onlyOwner() {
         require(_newOwner != address(0), "Owner address is zero address");
         ownerAddress = _newOwner;
     }
-
 }
-
